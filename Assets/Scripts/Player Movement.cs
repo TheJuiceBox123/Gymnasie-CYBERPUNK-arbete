7d5264322ef07465a86574c6534eef548c887845
TheJuiceBox123 <olundstrom08@gmail.com>
2026-09-10 15:03:17 +0200

    Fixat Basic Movement

--- Assets/Scripts/Player Movement.cs
+++ Assets/Scripts/Player Movement.cs
@@ -1,16 +1,45 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+
+    public float jumpForce;
+    
+    //InputAction jumpAction;
+    //InputAction moveAction;
+    Vector2 input;
+
     void Start()
     {
-        
+        //moveAction = InputSystem.actions.FindAction("Move");
     }
 
-    // Update is called once per frame
+    //private void FixedUpdate()
+    //{
+    //    input = moveAction.ReadValue<Vector2>();
+    //}
+
+    public void JumpInput(InputAction.CallbackContext context)
+    {
+        if (context.canceled) return;
+
+        rb.AddForce(Vector2.up * jumpForce * Time.fixedDeltaTime, ForceMode2D.Impulse);   
+        Debug.Log("Jumping");
+        // TODO: JUMP
+    }
+
+    public void MoveInput(InputAction.CallbackContext context)
+    {
+        input = context.ReadValue<Vector2>();
+        Debug.Log($"Move input: {input}");
+    }
+
     void Update()
     {
-        
+        transform.Translate(input * moveSpeed * Time.deltaTime);
     }
 }
